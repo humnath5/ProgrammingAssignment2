68c5e83db409bb8797add482d1841579f2f4bdcf
humnath5 <humnath.bh@gmail.com>
2015-05-22 14:03:19 -0500

    This is my version of R program for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+     
+                #//ProgrammingAssignment2#//
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#//
+     #makeCacheMatrix is a function which creates the "matrix" object.
+     # Return value of this function is a list of following four functions  
+     #set,get,setinverse and getinverse
+#//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#//
+      makeCacheMatrix<-function(x=matrix()){ 
+        
+         Inv<-NULL            
+         
+   #::::::::::caching  and receiving matrix variable "x" ::::::::::::  
+         set<-function(y){
+              x<<-y           
+              Inv<<- NULL
+            }  
+        get<-function() x     
+        
+   #::::::::::: caching and receiving inverse matrix "Inv" :::::::::::
+        setinverse<-function(inverse) 
+        Inv<<-inverse          
+        getinverse<-function()
+            Inv 
+   
+     list(set=set,get=get,setinverse=setinverse,
+             getinverse=getinverse)
+        }
+      
+  #//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#//
+       # cacheSolve function takes argument x which is a 
+       #list of functions created by makeCacheMatrix function
+       # It returns the inverse of matrix created by makeCacheMatrix
+ #//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#//
+      
+cacheSolve<-function(x,...){
+      
+  #:::::::: search for inverse if it was cache::::::::::::::::::::
+          Inv<-x$getinverse() 
+      if(!is.null(Inv)){ 
+             message("Getting Cache Data...")
+             return(Inv)
+             }
+  #:::::::::: getting matrix and checking  invertibility :::::::::   
+          data <-x$get() 
+           if(det(data)==0){           
+           message("Sorry, matrix is not invertible !!")
+           }
+  #::::::::computing and catching computed inverse:::::::::::::::;     
+           Inv<-solve(data)  
+           x$setinverse(Inv) 
+           Inv
+           
+   }
+      
+      
